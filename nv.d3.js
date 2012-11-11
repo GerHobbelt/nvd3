15125856fefc188844d566577689d3a0e2d61468
Jason Funk <jasonlfunk@gmail.com>
2012-11-11 16:55:23 -0600

    Add maxRows() option to legend which will allow for scrolling if items cannot be placed in the space alotted

--- nv.d3.js
+++ nv.d3.js
@@ -3126,6 +3126,7 @@ nv.models.legend = function() {
   var margin = {top: 5, right: 0, bottom: 5, left: 0}
     , width = 400
     , height = 20
+    , maxRows = false 
     , getKey = function(d) { return d.key }
     , color = nv.utils.defaultColor()
     , align = true
@@ -3147,8 +3148,8 @@ nv.models.legend = function() {
       var wrap = container.selectAll('g.nv-legend').data([data]);
       var gEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-legend').append('g');
       var g = wrap.select('g');
-
-      wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      var wrapTran = [margin.left, margin.top];
+      wrap.attr('transform', 'translate(' + wrapTran[0] + ',' + wrapTran[1] + ')');
 
       //------------------------------------------------------------
 
@@ -3189,52 +3190,145 @@ nv.models.legend = function() {
       if (align) {
         var seriesWidths = [];
         series.each(function(d,i) {
-              seriesWidths.push(d3.select(this).select('text').node().getComputedTextLength() + 28); // 28 is ~ the width of the circle plus some padding
-            });
+	      // 28 is ~ the width of the circle plus some padding
+              seriesWidths.push(d3.select(this).select('text').node().getComputedTextLength() + 28); 
+        });
 
         //nv.log('Series Widths: ', JSON.stringify(seriesWidths));
-
         var seriesPerRow = 0;
         var legendWidth = 0;
+        var legendHeight = 0;
         var columnWidths = [];
+	var xPositions = [];
+	var isColumnVisible = [];
 
-        while ( legendWidth < availableWidth && seriesPerRow < seriesWidths.length) {
-          columnWidths[seriesPerRow] = seriesWidths[seriesPerRow];
-          legendWidth += seriesWidths[seriesPerRow++];
-        }
+	while ( legendWidth < availableWidth && seriesPerRow < seriesWidths.length) {
+	  columnWidths[seriesPerRow] = seriesWidths[seriesPerRow];
+	  isColumnVisible[seriesPerRow] = true;
+	  legendWidth += seriesWidths[seriesPerRow++];
+	}
 
+	while ( legendWidth > availableWidth && seriesPerRow > 1 ) {
+	  columnWidths = [];
+	  seriesPerRow--;
 
-        while ( legendWidth > availableWidth && seriesPerRow > 1 ) {
-          columnWidths = [];
-          seriesPerRow--;
+	  for (k = 0; k < seriesWidths.length; k++) {
+	    if (seriesWidths[k] > (columnWidths[k % seriesPerRow] || 0) )
+	      columnWidths[k % seriesPerRow] = seriesWidths[k];
+	  }
 
-          for (k = 0; k < seriesWidths.length; k++) {
-            if (seriesWidths[k] > (columnWidths[k % seriesPerRow] || 0) )
-              columnWidths[k % seriesPerRow] = seriesWidths[k];
-          }
+	  legendWidth = columnWidths.reduce(function(prev, cur, index, array) {
+			  return prev + cur;
+	  });
+	}
+        var showLegendSlider = false;	
+	var numRows = Math.ceil(seriesWidths.length / seriesPerRow)+1
+	
+	// Check if our number of rows exceeds the maximum. If it does,
+	// recalculate things.
+	if (maxRows !== false && numRows > maxRows ) {
+		numRows = maxRows;
+		seriesPerRow = Math.ceil(seriesWidths.length/numRows);
+		for (var i = 0; i < seriesWidths.length; i++) {
+			var col = i % seriesPerRow 
+			if(columnWidths[col] === undefined) {
+				columnWidths[col] = 0;
+			}
+			columnWidths[col] = Math.max(seriesWidths[i],columnWidths[col])
+		};
+		legendWidth = 0;
+		for(i =0;i<seriesPerRow;i++) {
+			legendWidth += columnWidths[i];
+			if (legendWidth > availableWidth) {
+				isColumnVisible[i] = false;
+				showLegendSlider = true;
+			}
+		}
+	}
 
-          legendWidth = columnWidths.reduce(function(prev, cur, index, array) {
-                          return prev + cur;
-                        });
-        }
-        //console.log(columnWidths, legendWidth, seriesPerRow);
-
-        var xPositions = [];
-        for (var i = 0, curX = 0; i < seriesPerRow; i++) {
-            xPositions[i] = curX;
-            curX += columnWidths[i];
-        }
+	for (var i = 0, curX = 0; i < seriesPerRow; i++) {
+	    xPositions[i] = curX;
+	    curX += columnWidths[i];
+	}
 
         series
             .attr('transform', function(d, i) {
               return 'translate(' + xPositions[i % seriesPerRow] + ',' + (5 + Math.floor(i / seriesPerRow) * 20) + ')';
             });
+        series
+            .style('display',function(d, i) {
+	    	return isColumnVisible[ i % seriesPerRow] ? 'block' : 'none';
+	    });
 
         //position legend as far right as possible within the total width
-        g.attr('transform', 'translate(' + (width - margin.right - legendWidth) + ',' + margin.top + ')');
+        g.attr('transform', 'translate(' + Math.max(0,width - margin.right - legendWidth) + ',' + margin.top + ')');
 
         height = margin.top + margin.bottom + (Math.ceil(seriesWidths.length / seriesPerRow) * 20);
-
+	if (showLegendSlider) {
+      		var elm1 = container.append("svg:path").attr("class","nv-legend-right").style("cursor","pointer")
+		    	.attr("d", d3.svg.symbol().type("triangle-down").size(32))
+			.attr("transform", 'translate(' + availableWidth + ',' + (numRows * 20)/2 + ') rotate(-90)');
+      		var elm2 = container.append("svg:path").attr("class","nv-legend-left").style("cursor","pointer")
+		    	.attr("d", d3.svg.symbol().type("triangle-down").size(32))
+			.attr("transform", 'translate(-20,' + (numRows * 20)/2 + ') rotate(90)')
+			.style("display","none");
+          	container.selectAll("[class^=nv-legend-]").on('click', function() {
+				var left = d3.select(this).attr("class") == "nv-legend-left";
+          			var mode = 0;
+				var toHideIdx = -1;
+				var toShowIdx = -1;
+				if (!left) {
+					if (isColumnVisible[isColumnVisible.length-1]) { return}
+					//Loop through column visiblities and make the first visible
+					//one hidden and the first hidden on visble
+					for (var i = 0; i < isColumnVisible.length; i++) {
+						if (mode == 0 && isColumnVisible[i]) {
+							isColumnVisible[i] = false;;
+							toHideIdx = i;
+							mode = 1;
+						} else if(mode == 1 && !isColumnVisible[i]) {
+							isColumnVisible[i] = true;;
+							toShowIdx = i;
+							break;
+						}
+					};
+			       		wrapTran[0] -= columnWidths[toHideIdx];
+				} else {
+					if (isColumnVisible[0]) { return}
+					//Loop through column visiblities and make the first last invisible
+					//one visible and the last visble one hidden
+					for (var i = 0; i < isColumnVisible.length; i++) {
+						if (mode == 0 && isColumnVisible[i]) {
+							isColumnVisible[i-1] = true;
+							toShowIdx = i-1;
+							mode = 1;
+						} else if(mode == 1 && !isColumnVisible[i]) {
+							isColumnVisible[i-1] = false;
+							toHideIdx = i-1;
+							break;
+						}
+					};
+					if (toHideIdx == -1) {
+						isColumnVisible[isColumnVisible.length-1] = false;
+					}
+			       		wrapTran[0] += columnWidths[toShowIdx];
+				}
+				series.style("display",function(d, i) {
+					return isColumnVisible[i % seriesPerRow] ? "block" : "none";
+				});
+				if (isColumnVisible[0]) {
+					container.select(".nv-legend-left").style("display","none");
+				} else {
+					container.select(".nv-legend-left").style("display","block");
+				}
+				if (isColumnVisible[isColumnVisible.length-1]) {
+					container.select(".nv-legend-right").style("display","none");
+				}else{
+					container.select(".nv-legend-right").style("display","block");
+				}
+			       wrap.attr('transform', 'translate(' + wrapTran[0] + ',' + wrapTran[1] + ')');
+			})
+	}
       } else {
 
         var ypos = 5,
@@ -3294,6 +3388,12 @@ nv.models.legend = function() {
   chart.height = function(_) {
     if (!arguments.length) return height;
     height = _;
+    return chart;
+  };
+  
+  chart.maxRows = function(_) {
+    if (!arguments.length) return maxRows;
+    maxRows = _;
     return chart;
   };
 
